2430b02b12d1e1fc26368f3b9deb6e6cb63a2d14
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-15 17:42:19 -0300

    ok

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,7 +1,48 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export const Container = () => {
+
+    const [info,setInfo] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    const fetchData = async () => {
+        const res = await axios.get('https://relatorio-ivt.herokuapp.com/api/listall')
+        const data = await res.data;
+        setInfo(data);
+        console.log(info);
+    }
+
     return (
         <div>
-            
+            {info.map((item)=>(
+                <>
+            <div key="01">
+                {item.name}
+            </div>
+            <div key="02">
+                {item.clientes}
+            </div>
+            <div key="03">
+                {item.fundos}
+            </div>
+            <div key="04">
+                {item.reuniao}
+            </div>
+            <div key="05">
+                {item.sistema}
+            </div>
+            <div key="06">
+                {item.performit}
+            </div>
+            <div key="07">
+                {item.conhecimento}
+            </div>
+            </>
+            ))}
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Container } from "../components/Container/Container";
 import { Header } from "../components/Header/Header";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Container/>
     </div>
   )
 }
